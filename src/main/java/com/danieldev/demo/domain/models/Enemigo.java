738ec99220c0d77d762587ec8063e078e51bc39c
DanielDev <daniel.agudelomo@amigo.du.co>
2026-08-26 09:30:44 -0500

    Se crea la clase hija enemigo

--- src/main/java/com/danieldev/demo/domain/models/Enemigo.java
+++ src/main/java/com/danieldev/demo/domain/models/Enemigo.java
@@ -1,5 +1,20 @@
 package com.danieldev.demo.domain.models;
 
-public class Enemigo {
+import com.danieldev.demo.domain.enums.TipoElemento;
+
+public class Enemigo extends Personaje {
+    private final TipoElemento tipoElemento;
+
+    public Enemigo(String nombre, int puntosVida, int puntosAtaque, TipoElemento tipoElemento) {
+        super(nombre, puntosVida, puntosAtaque);
+        this.tipoElemento = tipoElemento;
+    }
+    @Override
+    public int calcaularDanio(){
+    return  (int) (this.puntosAtaque * tipoElemento.getMultiplicador());
+    }
+    public TipoElemento getTipoElemento() {
+        return tipoElemento;
+    }
 
 }
